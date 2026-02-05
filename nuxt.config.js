bf6d97758ee2d545d6968a245bc6d6d8135ec73d
qldwin <elonyx7@gmail.com>
2026-02-05 18:45:55 +0100

    chore: update commander and nypm versions in package-lock.json; remove compatibility date from nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
 export default defineNuxtConfig({
-    compatibilityDate: "2024-11-01",
     devtools: { enabled: true },
     css: ["~/assets/css/styles.css"],
     modules: ["@nuxt/ui", "nuxt-auth-utils", "@nuxt/eslint"],
