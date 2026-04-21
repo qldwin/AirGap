7dc427c16ddbc4d40e82410e198df66987413be8
aldwin <elonyx7@gmail.com>
2026-04-21 18:44:16 +0200

    fix: improve JWT authentication by validating user session against the database

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,4 +1,7 @@
 import type {H3Event} from 'h3'
+import {eq} from "drizzle-orm";
+import {db} from "#server/db";
+import {users} from "~~/drizzle/schema";
 
 declare module '#auth-utils' {
     interface User {
@@ -16,11 +19,25 @@ export interface UserSession {
 
 export const requireAuth = async (event: H3Event): Promise<UserSession> => {
     const session = await getUserSession(event)
+    console.log('User session:', session)
     if (!session?.user) {
         throw createError({
             statusCode: 401,
             message: 'Vous devez être connecté pour effectuer cette action'
         })
     }
+
+    const dbUser = await db.query.users.findFirst({
+        where: eq(users.id, session.user.id)
+    });
+
+    if (!dbUser) {
+        await clearUserSession(event);
+        throw createError({
+            statusCode: 401,
+            message: 'Session invalide ou compte supprimé'
+        });
+    }
+
     return session.user as UserSession
 }
